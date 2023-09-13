b27cb538de8d39cb7ca862a101985edea30b8a75
HJunng <guswjd1gh@gmail.com>
2023-09-14 02:42:11 +0900

    Add components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      hello from ExpenseItem
+    </div>
+  )
+};
+
+export default ExpenseItem
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from './ExpenseItem'
+const ExpenseList = () => {
+  return (
+    <div>
+      hello from ExpenseList
+      <Item />
+    </div>
+  )
+};
+
+export default ExpenseList;

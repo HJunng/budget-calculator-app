9edf4bbb758dd77c005db6952d47642aca0c4c9b
HJunng <guswjd1gh@gmail.com>
2023-09-14 12:23:32 +0900

    submit, danger 알람 생성

--- src/App.js
+++ src/App.js
@@ -28,26 +28,38 @@ function App() {
   const [charge,setCharge] = useState('');
   // single amout
   const [amount,setAmount] = useState('');
-  // //alert
-  // const [alert,setAlert] = useState({show:false});
+  //alert
+  const [alert,setAlert] = useState({show:false});
   //*********** functionality **********/
   const handleCharge = (e) =>{
     setCharge(e.target.value);
   }
   const handleAmount = (e) =>{
     setAmount(e.target.value);
+  };
+  // handle alert
+  const handleAlert = ({type,text}) => {
+    setAlert({show:true,type,text});
+    setTimeout(() => {
+      setAlert({show:false})
+    },3000)
   }
+  //handle submit
   const handleSubmit = (e) =>{
     
     e.preventDefault();
     if(charge !== '' && amount>0){
       const singleExpense = {id:uuid(),charge,amount};
       setExpenses([...expenses,singleExpense]);
+      // setExpenses(...[expenses],[singleExpense]) 아님..!
+
+      handleAlert({type:'success',text:'지출 내역이 생성되었습니다'});
+
       setCharge('');
       setAmount('');
-      // setExpenses(...[expenses],[singleExpense]) 아님..!
     }else{
       //handle alert called
+      handleAlert({type:'danger',text:'값을 입력해주세요'})
     }
   }
 
@@ -58,8 +70,8 @@ function App() {
 
   return (
     <>
-      {/* {alert.show && <Alert type={alert.type} text={alert.text} />}
-      <Alert /> */}
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <Alert />
       <h1>예산 계산기</h1>
       <main className='App'>
         <ExpenseForm charge={charge} amount={amount} handleCharge={handleCharge} handleAmount={handleAmount} handleSubmit={handleSubmit} />

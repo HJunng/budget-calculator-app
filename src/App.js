987bad53948ad372458f32e94291666f3d2e7cb6
HJunng <guswjd1gh@gmail.com>
2023-09-14 12:12:03 +0900

    form 제출 시 list 반영

--- src/App.js
+++ src/App.js
@@ -21,23 +21,53 @@ const initialExpenses = [
 //function for updates/control
 //default value
 function App() {
-  //console.log(useState());
+  //*********** state values **********/
+  // all expenses, add expense
   const [expenses,setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge,setCharge] = useState('');
+  // single amout
+  const [amount,setAmount] = useState('');
+  // //alert
+  // const [alert,setAlert] = useState({show:false});
+  //*********** functionality **********/
+  const handleCharge = (e) =>{
+    setCharge(e.target.value);
+  }
+  const handleAmount = (e) =>{
+    setAmount(e.target.value);
+  }
+  const handleSubmit = (e) =>{
+    
+    e.preventDefault();
+    if(charge !== '' && amount>0){
+      const singleExpense = {id:uuid(),charge,amount};
+      setExpenses([...expenses,singleExpense]);
+      setCharge('');
+      setAmount('');
+      // setExpenses(...[expenses],[singleExpense]) 아님..!
+    }else{
+      //handle alert called
+    }
+  }
+
+
   console.log(expenses);
   //useState(initialExpenses) 0번째에는 Array, 1번째에는 function이 들어있음.
   
 
   return (
     <>
-      <Alert />
+      {/* {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <Alert /> */}
       <h1>예산 계산기</h1>
       <main className='App'>
-        <ExpenseForm />
+        <ExpenseForm charge={charge} amount={amount} handleCharge={handleCharge} handleAmount={handleAmount} handleSubmit={handleSubmit} />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>총 지출 :  <span className='total'>
         {expenses.reduce((acc,curr)=>{
-          return acc+=curr.amount;
+          return acc+=parseInt(curr.amount);
         },0)}
       </span> 원 </h1>
     </>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {MdSend} from 'react-icons/md';
 
-const ExpenseForm = () => {
-  return <form>
+const ExpenseForm = ({charge,amount, handleCharge, handleAmount, handleSubmit}) => {
+  return <form onSubmit={handleSubmit}>
     <div className='form-center'>
       <div className='form-group'>
         <label htmlFor='expense'>지출 항목</label>
-        <input type='text' className='form-control' id='charge' name='charge' placeholder='예) 렌트비'/>
+        <input type='text' className='form-control' id='charge' name='charge' placeholder='예) 렌트비' value={charge} onChange={handleCharge}/>
       </div>
       <div className='form-group'>
         <label htmlFor='amount'>비용</label>
-        <input type='text' className='form-control' id='amount' name='amout' placeholder='예) 10000'/>
+        <input type='text' className='form-control' id='amount' name='amout' placeholder='예) 10000' value={amount} onChange={handleAmount}/>
       </div>
     </div>
     <button type='submit' className='btn'>
